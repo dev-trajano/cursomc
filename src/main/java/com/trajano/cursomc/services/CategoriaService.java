06644c1199696886f9bf5735da2ef8ebd9c96d2f
Joanderson Trajano <trajanojoanderson@gmail.com>
2022-07-25 16:03:01 -0300

    Inserindo uma nova categoria com POST

--- src/main/java/com/trajano/cursomc/services/CategoriaService.java
+++ src/main/java/com/trajano/cursomc/services/CategoriaService.java
@@ -18,7 +18,13 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		}
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+
+	}
 
 }
